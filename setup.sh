07dbb64fd678aacf82e321d5c5b0718752548255
cchatzis <cchatzis@outlook.com>
2025-02-13 17:19:40 +0100

    Added setup script and requirements

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Delete virtual environment folder if it exists
+if [ -d "myenv" ]; then
+    rm -rf myenv
+fi
+
+#chmod
+chmod u+rwx $(pwd)
+
+# Create and activate virtual environment
+python3.10 -m venv myenv
+source myenv/bin/activate
+
+# Install dependencies
+pip install --no-cache-dir -r requirements.txt
+
+pip install jupyter ipykernel notebook
+python -m ipykernel install --user --name=myenv --display-name "Python (myenv)"
+
+# Launch Jupyter Notebook
+jupyter-notebook --NotebookApp.use_redirect_file=False dCMF.ipynb --debug
